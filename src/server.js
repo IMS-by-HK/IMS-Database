b294268781e8039a6df56597e282dc5cbadfe9ef
Kate G <161684914+k8-g@users.noreply.github.com>
2024-12-19 14:04:59 +1000

    Update server.js

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@
 // - connections to file storage 
 
 const express = require("express");
-// const cors = require('cors');
+
 const app = express();
 
 
@@ -21,13 +21,7 @@ app.use(require('./controllers/UserController'));
 
 app.use(express.json());
 
-// Enables CORS
-// let corsOptions = {
-//     //          CRA local                  local 					Vite local              Vite local              Deployed React app
-//     origin: ["http://localhost:3000", "http://localhost:8080", "http://localhost:5173", "http://127.0.0.1:5173","https://ims-hk.netlify.app/"],
-//     optionsSuccessStatus: 200
-// };
-// app.use(cors(corsOptions));
+
 
 // app.verb(path, callback);
 app.get("/", (request, response) => {
